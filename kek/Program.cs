8acadc56382f939948ce4fb5d5380234190fef1e
Кирилл <vtorcevkirill@mail.ru>
2023-04-13 19:03:57 +0400

    Задание 6.3.2 Для класса Bus реализуйте метод PrintStatus, который будет сообщать о количестве пассажиров в автобусе, если они есть. Или сообщать, что автобус пуст.

--- kek/Program.cs
+++ kek/Program.cs
@@ -5,48 +5,22 @@
 
         static void Main(string[] args)
         {
-            var department = GetCurrentDepartment();
-            static Department GetCurrentDepartment()
-            {
-                // logic
-            }
-            /*Напишите такой код, который бы 
-             * при типе компании, равному типу "Банк", и городе "Санкт-Петербург" 
-             * выводил в консоль сообщение "У банка ??? есть отделение в Санкт-Петербурге", 
-             * где вместо "???" выводилось бы название компании.
-
-            Если у компании нет названия, вместо него должно быть "Неизвестная компания".*/
-
-            if (department?.Company.Type == "Банк" && department?.City.Name == "Санкт-Петербург")
-            {
-                string s = "У банка {0} есть отделение в Санкт-Петербурге";
-                Console.WriteLine(s, department.Company.Name);
-            }
-            else
-            {
-                Console.WriteLine("Неизвестная компания");
-            }
-            if (department?.Company.Type == "Банк" && department?.City.Name == "Санкт-Петербург")
-            {
-                string s = "У банка {0} есть отделение в Санкт-Петербурге";
-                Console.WriteLine(s, department?.Company?.Name ?? "Неизвестная компания");
-            }
+            var bus = new Bus();
+            bus.Load = 0;
+            bus.PrintStatus();
         }
-        class Company
+        class Bus
         {
-            public string Type;
-            public string Name;
-        }
+            public int? Load;
 
-        class Department
-        {
-            public Company Company;
-            public City City;
-        }
-
-        class City
-        {
-            public string Name;
+            public void PrintStatus()
+            {
+                if (this.Load != null && this.Load > 0)
+                    Console.WriteLine("В автобусте: {0}", this.Load);
+                else 
+                    Console.WriteLine("Автобус пуст");
+                
+            }
         }
 
     }
